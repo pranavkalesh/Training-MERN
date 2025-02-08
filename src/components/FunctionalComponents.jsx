28691321a408608c24638ef81354910e9c3588e4
pranavkalesh <221cs010503@mallareddyuniversity.ac.in>
2025-02-08 09:23:28 +0530

    day 07/02

---
+++ src/components/FunctionalComponents.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// impport './myStyles.csss';
+
+function FunctionalComponents(props) {
+    render() 
+        return (
+            <div className='App'>
+            <h1 class='heding'>Hello this is sucessfull</h1>
+             <h2 class={StyleSheet.erroe}>Errorr</h2>
+             <FunctionalComponents />   
+            </div>
+        );
+    }
+
+
+export default FunctionalComponents;
